81f9c33365a6633c4aa487b085b40b3b73c9f505
Wang <Harry-CC.Wang@aia.com>
2026-07-15 12:01:23 +0800

    added while loop

--- learning_cpp/terminal_based_task_manager/main.cpp
+++ learning_cpp/terminal_based_task_manager/main.cpp
@@ -3,33 +3,58 @@
 #include <string>
 #include <vector>
 
-void addTasks(std::vector<std::string>& tasks, const std::string& name);
+void startTaskManager();
+
+void addTasks(std::vector<std::string>& tasks);
 
 void displayTasks(const std::vector<std::string>& tasks);
 
+
 int main() {
-    std::string name;
-    std::vector<std::string> tasks;
-
-    std::cout << "What is your name? ";
-    std::getline(std::cin, name);
-
-    addTasks(tasks, name);
-    displayTasks(tasks);
-
+    startTaskManager();
     return 0;
 }
 
-void addTasks(std::vector<std::string>& tasks, const std::string& name) {
+void startTaskManager() {
+    std::vector<std::string> tasks;
+
+    while (true){
+        int choice_no;
+
+        std::cout << "===== Task Manager =====\n" 
+            << "1. Add tasks\n"
+            << "2. Display tasks\n"
+            << "3. Exit\n"
+            << "Enter your choice: ";
+
+        std::cin >> choice_no;
+
+        // need to check input from user
+        if (choice_no <= 0 || choice_no > 3) {
+            std::cout << "GET OUT!" << std::endl;
+            break;
+        }
+        
+        // add tasks
+        if (choice_no == 1) {
+            addTasks(tasks);
+        } else if (choice_no == 2) {
+            displayTasks(tasks);
+        } else if (choice_no == 3) {
+            break;
+        }
+    }
+
+    std::cout << "GET OUT!" << std::endl;
+}
+
+void addTasks(std::vector<std::string>& tasks) {
     int taskCount = 0;
 
-    std::cout << "Hello " << name
-              << ". How many tasks do you want to complete today? ";
-
+    std::cout << "Hello there. How many tasks do you want to complete today? ";
     std::cin >> taskCount;
 
-    if (taskCount < 1 || taskCount > 10)
-    {
+    if (taskCount < 1 || taskCount > 10) {
         std::cout << "Please enter a number between 1 and 10.\n";
         return;
     }
@@ -39,8 +64,7 @@ void addTasks(std::vector<std::string>& tasks, const std::string& name) {
         '\n'
     );
 
-    for (int i = 1; i <= taskCount; ++i)
-    {
+    for (int i = 1; i <= taskCount; ++i) {
         std::string task;
 
         std::cout << "Task " << i << ": ";
@@ -51,16 +75,14 @@ void addTasks(std::vector<std::string>& tasks, const std::string& name) {
 }
 
 void displayTasks(const std::vector<std::string>& tasks) {
-    if (tasks.empty())
-    {
+    if (tasks.empty()) {
         std::cout << "No tasks were added.\n";
         return;
     }
 
     std::cout << "\nThese are your tasks:\n";
 
-    for (std::size_t i = 0; i < tasks.size(); ++i)
-    {
+    for (std::size_t i = 0; i < tasks.size(); ++i) {
         std::cout << "Task " << i + 1
                   << ": " << tasks[i] << '\n';
     }
