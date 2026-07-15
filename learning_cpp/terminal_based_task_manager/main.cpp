bf203bfa9b243483d0386613b693faa1cba0e4c4
Wang <Harry-CC.Wang@aia.com>
2026-07-15 15:38:45 +0800

    remove task

--- learning_cpp/terminal_based_task_manager/main.cpp
+++ learning_cpp/terminal_based_task_manager/main.cpp
@@ -7,6 +7,8 @@ void startTaskManager();
 
 void addTasks(std::vector<std::string>& tasks);
 
+void deleteTask(std::vector<std::string>& tasks);
+
 void displayTasks(const std::vector<std::string>& tasks);
 
 
@@ -19,28 +21,44 @@ void startTaskManager() {
     std::vector<std::string> tasks;
 
     while (true){
-        int choice_no;
+        int choiceNo;
 
         std::cout << "===== Task Manager =====\n" 
             << "1. Add tasks\n"
             << "2. Display tasks\n"
-            << "3. Exit\n"
+            << "3. Delete task\n"
+            << "4. Exit\n"
             << "Enter your choice: ";
 
-        std::cin >> choice_no;
+        if (!(std::cin >> choiceNo)) {
+            std::cout << "Please enter a number.\n";
+
+            std::cin.clear();
+            std::cin.ignore(
+                std::numeric_limits<std::streamsize>::max(),
+                '\n'
+            );
+
+            continue;
+        }
 
         // need to check input from user
-        if (choice_no <= 0 || choice_no > 3) {
+        if (choiceNo <= 0 || choiceNo > 4) {
             std::cout << "GET OUT!" << std::endl;
-            break;
+            continue;
         }
         
         // add tasks
-        if (choice_no == 1) {
+        if (choiceNo == 1) {
             addTasks(tasks);
-        } else if (choice_no == 2) {
+        } 
+        if (choiceNo == 2) {
             displayTasks(tasks);
-        } else if (choice_no == 3) {
+        }
+        if (choiceNo == 3)  {
+            deleteTask(tasks);
+        } 
+        if (choiceNo == 4) {
             break;
         }
     }
@@ -52,7 +70,17 @@ void addTasks(std::vector<std::string>& tasks) {
     int taskCount = 0;
 
     std::cout << "Hello there. How many tasks do you want to complete today? ";
-    std::cin >> taskCount;
+    if (!(std::cin >> taskCount)) {
+        std::cout << "Please enter a number.\n";
+
+        std::cin.clear();
+        std::cin.ignore(
+            std::numeric_limits<std::streamsize>::max(),
+            '\n'
+        );
+
+        return;
+    }
 
     if (taskCount < 1 || taskCount > 10) {
         std::cout << "Please enter a number between 1 and 10.\n";
@@ -73,6 +101,41 @@ void addTasks(std::vector<std::string>& tasks) {
         tasks.push_back(task);
     }
 }
+
+void deleteTask(std::vector<std::string>& tasks) {
+    if (tasks.empty()) {
+        std::cout << "No tasks available! \n";
+        return;
+    }
+    std::cout << "Tasks: \n";
+    for (size_t i = 0; i < tasks.size(); i++) std::cout << i + 1 << ". " << tasks[i] << "\n";
+
+    int idx;
+    std::cout << "Enter Task number to remove: ";
+
+    if (!(std::cin >> idx)) {
+        std::cout << "Please enter a number.\n";
+
+        std::cin.clear();
+        std::cin.ignore(
+            std::numeric_limits<std::streamsize>::max(),
+            '\n'
+        );
+
+        return;
+    }
+    
+    if (idx <= 0 || idx > static_cast<int>(tasks.size())) {
+        std::cout << "GET OUT";
+        return;
+    }
+
+    tasks.erase(tasks.begin() + idx-1);
+    std::cout << "Task removed. \n";
+
+    displayTasks(tasks);
+}
+
 
 void displayTasks(const std::vector<std::string>& tasks) {
     if (tasks.empty()) {
