bfc29f4d21a74aafc3d48fdb18b0277d3c1fc8b1
Your Name <you@example.com>
2026-08-24 02:28:24 +0000

    testing the build

---
+++ learning_cpp/matching_engine/build/CMakeFiles/matching_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matching_engine.dir/src/OrderBook.cpp.o"
+  "CMakeFiles/matching_engine.dir/src/OrderBook.cpp.o.d"
+  "CMakeFiles/matching_engine.dir/src/main.cpp.o"
+  "CMakeFiles/matching_engine.dir/src/main.cpp.o.d"
+  "matching_engine"
+  "matching_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matching_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
